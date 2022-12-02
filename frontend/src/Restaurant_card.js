d6597e3b4647a547f1746f1efb30ef300b7c022a
qui900826 <71548347+qui900826@users.noreply.github.com>
2022-12-03 02:31:46 +0800

    Add files via upload

--- frontend/src/Restaurant_card.js
+++ frontend/src/Restaurant_card.js
@@ -1,14 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-
-import { BsFillCaretDownFill  } from "react-icons/bs";
-
-import './All.css'
+import './all.css'
 import images from './images/pexels-chan-walrus-958545.jpg';
 
 function Restaurant_card() {
@@ -18,43 +14,85 @@ function Restaurant_card() {
         <Card.Img variant="top" src={images}/>
         <Card.Body>
           <Card.Text>
-            餐廳名稱
+             餐廳名稱
           </Card.Text>
+          
+          <Accordion defaultActiveKey="1">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>
+                <Badge pill bg=" " text="dark">
+                  距離 5 KM
+                </Badge>{' '}
+                <Badge pill bg=" " text="dark">
+                  價格 200 NT
+                </Badge>
+              </Accordion.Header>
+              <Accordion.Body>
+                <Card className="text-center">
+                  <Card.Header>由 0816148 發起</Card.Header>
+                  <Card.Body>
+                    <Button variant="dark">一起拼單 GO</Button>
+                  </Card.Body>
+                  <Card.Footer className="text-muted">在 20 分鐘後送出訂單</Card.Footer>
+                </Card>
 
-          <>
-            <div>
-              {[DropdownButton].map((DropdownType, idx) => (
-                <DropdownType
-                  as={ButtonGroup}
-                  key={idx}
-                  id={`dropdown-button-drop-${idx}`}
-                  size="sm"
-                  variant=""
-                  title={
-                    <div>
-                      <Badge pill bg="light" text="dark">
-                        距離1KM
-                      </Badge>{' '}
-                      <Badge pill bg="light" text="dark">
-                        價格200NT
-                      </Badge>{' '}
-                      <Badge pill bg="light" text="dark">
-                      <div className = "icon">
-                        <BsFillCaretDownFill />
-                      </div>
-                      </Badge>
-                    </div>
-                  }
-                >
-                  <Dropdown.Item eventKey="1">123</Dropdown.Item>
-                  <Dropdown.Item eventKey="2">123</Dropdown.Item>
-                </DropdownType>
-              ))}
-            </div>
-          </>
+                <br></br>
+
+                <Card className="text-center">
+                  <Card.Header>由 0816148 發起</Card.Header>
+                  <Card.Body>
+                    <Button variant="dark">一起拼單 GO</Button>
+                  </Card.Body>
+                  <Card.Footer className="text-muted">在 20 分鐘後送出訂單</Card.Footer>
+                </Card>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+
         </Card.Body>
       </Card>
       <br/>
+      <Card>
+        <Card.Img variant="top" src={images}/>
+        <Card.Body>
+          <Card.Text>
+             餐廳名稱
+          </Card.Text>
+          
+          <Accordion defaultActiveKey="1">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>
+                <Badge pill bg=" " text="dark">
+                  距離 5 KM
+                </Badge>{' '}
+                <Badge pill bg=" " text="dark">
+                  價格 200 NT
+                </Badge>
+              </Accordion.Header>
+              <Accordion.Body>
+                <Card className="text-center">
+                  <Card.Header>由 0816148 發起</Card.Header>
+                  <Card.Body>
+                    <Button variant="dark">一起拼單 GO</Button>
+                  </Card.Body>
+                  <Card.Footer className="text-muted">在 20 分鐘後送出訂單</Card.Footer>
+                </Card>
+
+                <br></br>
+                
+                <Card className="text-center">
+                  <Card.Header>由 0816148 發起</Card.Header>
+                  <Card.Body>
+                    <Button variant="dark">一起拼單 GO</Button>
+                  </Card.Body>
+                  <Card.Footer className="text-muted">在 20 分鐘後送出訂單</Card.Footer>
+                </Card>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+
+        </Card.Body>
+      </Card>
     </>
   );
 }
