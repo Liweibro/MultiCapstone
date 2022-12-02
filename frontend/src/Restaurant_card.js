5bd2c8655ea9de446c909004833f0417c305ed07
qui900826 <71548347+qui900826@users.noreply.github.com>
2022-12-02 21:34:36 +0800

    Add files via upload

--- frontend/src/Restaurant_card.js
+++ frontend/src/Restaurant_card.js
@@ -1,15 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
-import Accordion from 'react-bootstrap/Accordion';
+
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+import { BsFillCaretDownFill  } from "react-icons/bs";
 
 import './All.css'
 import images from './images/pexels-chan-walrus-958545.jpg';
 
 function Restaurant_card() {
   return (
-    <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
     <>
       <Card>
         <Card.Img variant="top" src={images}/>
@@ -18,38 +21,41 @@ function Restaurant_card() {
             餐廳名稱
           </Card.Text>
 
-          <div>
-            <Badge pill bg="light" text="dark">
-              距離1KM
-            </Badge>{' '}
-            <Badge pill bg="light" text="dark">
-              價格200NT
-            </Badge>
-          </div>
-
-          <id class="toggle">
-            <Accordion.Header></Accordion.Header> 
-          </id>
-          
-            
-              
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
-              </Accordion.Body>
-            
-          
+          <>
+            <div>
+              {[DropdownButton].map((DropdownType, idx) => (
+                <DropdownType
+                  as={ButtonGroup}
+                  key={idx}
+                  id={`dropdown-button-drop-${idx}`}
+                  size="sm"
+                  variant=""
+                  title={
+                    <div>
+                      <Badge pill bg="light" text="dark">
+                        距離1KM
+                      </Badge>{' '}
+                      <Badge pill bg="light" text="dark">
+                        價格200NT
+                      </Badge>{' '}
+                      <Badge pill bg="light" text="dark">
+                      <div className = "icon">
+                        <BsFillCaretDownFill />
+                      </div>
+                      </Badge>
+                    </div>
+                  }
+                >
+                  <Dropdown.Item eventKey="1">123</Dropdown.Item>
+                  <Dropdown.Item eventKey="2">123</Dropdown.Item>
+                </DropdownType>
+              ))}
+            </div>
+          </>
         </Card.Body>
       </Card>
       <br/>
     </>
-    </Accordion.Item>
-    </Accordion>
   );
 }
 
