d5a13ed0603dd32f4f430003e673a5c64d7e92c3
pocky08 <119866955+pocky08@users.noreply.github.com>
2022-12-15 11:53:35 +0800

    Update Item.js

--- frontend/src/src/my-orders/components/Item.js
+++ frontend/src/src/my-orders/components/Item.js
@@ -1,26 +1,29 @@
 import { Grid } from "@mui/material";
 import Image from "react-bootstrap/Image";
+import { Link } from "react-router-dom";
 
 const Item = ({}) => {
   return (
-    <button className="group_button">
-      <Grid container>
+    <Link to="/myorderdata">
+      <button className="group_button">
         <Grid container>
-          <Grid xs={3}>
-            <div>
-              <Image
-                src=
-                "https://media.geeksforgeeks.org/wp-content/uploads/20210425000233/test-300x297.png"
-                roundedCircle width="70"
-              />
-            </div>
-          </Grid>
-          <Grid xs={7} className={"mygroup_name"}>
-            多媒吃飯糰
+          <Grid container>
+            <Grid xs={3}>
+              <div>
+                <Image
+                  src=
+                  "https://media.geeksforgeeks.org/wp-content/uploads/20210425000233/test-300x297.png"
+                  roundedCircle width="70"
+                />
+              </div>
+            </Grid>
+            <Grid xs={7} className={"mygroup_name"}>
+              多媒吃飯糰
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </button>
+      </button>
+    </Link>
   );
 };
 
