562184d380194097e205631f98d9dae5d855b47d
pocky08 <119866955+pocky08@users.noreply.github.com>
2022-12-15 11:50:27 +0800

    Update Edit.js

--- frontend/src/src/order-meals/components/Edit.js
+++ frontend/src/src/order-meals/components/Edit.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { v4 } from "uuid";
-
+import { Link } from "react-router-dom";
 const Edit = ({}) => {
   return (
     <div>
-      <button className="buy" style={{ fontSize: 22 }}>
-        加入購物車
-      </button>
+    　<Link to="/allmodal">
+        <button className="buy" style={{ fontSize: 22 }}>
+          加入購物車
+        </button>
+      </Link>
     </div>
   );
 };
