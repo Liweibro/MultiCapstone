6737bfd3f165c9aa7e944d7cfd6283438643d024
pocky08 <119866955+pocky08@users.noreply.github.com>
2022-12-24 01:36:28 +0800

    Update MyOrder.jsx
    add{order.d.xxx}

--- frontend/src/src/OrderData/MyOrder.jsx
+++ frontend/src/src/OrderData/MyOrder.jsx
@@ -1,11 +1,25 @@
 import React, {useState} from 'react';
+import {useLocation} from 'react-router-dom';
 import './MyOrder.css';
 import { 
     Button,
     } from 'react-bootstrap';
 import {ChatText, ArrowLeftCircle} from "react-bootstrap-icons";
 
+function GetTime(props) {
+    var myDate = new Date(props.time);
+    return (
+      <div>{myDate.getHours()}：{myDate.getMinutes()}</div>
+    );
+}
+
 function MyOrder() {
+    const location = useLocation()
+    console.log(location.state.order)
+    const order = location.state.order;
+
+    const counters = Array.from({ length: order.d.order_num }); 
+
     return (
         <>
         <div className='Back'><ArrowLeftCircle size={30}/></div>
@@ -16,48 +30,46 @@ function MyOrder() {
                 <div class="col" />
 
                 <div class="col-9">
-                    <div class="container top-container"> {/* 上方 */}
+                    <div class="top-container"> {/* 上方 */}
                         <div class="row row-cols-auto justify-content-center">
                             <div class="col-3 text-right">人數：</div>
-                            <div className='NumberBox'>3</div>
+                            <div className='NumberBox'>{order.d.human_lowerbound}</div>
                             <div class="col-1">~</div>
-                            <div className='NumberBox'>5</div>
+                            <div className='NumberBox'>{order.d.human_upperbound}</div>
                         </div>
                         <div class="row row-cols-auto justify-content-center">
                             <div class="col-4">送餐時間：</div>
-                            <div class="col-6 TextBox"></div>
+                            <div class="col-6 TextBox">{order.d.autosend?<GetTime time={order.d.autosend_time.seconds*1000}/>:"未設定自動送出"}</div>
                         </div>
                         <div class="row row-cols-auto justify-content-center" style={{ marginBottom: "4%" }}>
                             <div class="col-4">取餐地點：</div>
-                            <div class="col-6 TextBox"></div>
+                            <div class="col-6 TextBox">{order.d.dest}</div>
                         </div>
                     </div>
 
                     <div class="row">
-                        <div className='ResName'>餐廳名稱</div>
+                        <div className='ResName'>{order.d.restaurant_name}</div>
                     </div>
 
                     <div class="row row-cols-auto justify-content-between">
                         <div>總人數：</div>
-                        <div className='NumberBox'>4</div>
+                        <div className='NumberBox'>{order.d.order_num}</div>
 
                         <div>總價格：</div>
-                        <div className='NumberBox' style={{ width: "4em" }}>360元</div>
+                        <div className='NumberBox' style={{ width: "4em" }}>{order.d.sum_price}</div>
                     </div>
 
                     <br></br>
-                    <div class="container bottom-container"> {/* 下方 */}
-                        <div class="row justify-content-center">
-                            <div class="col-10 UserBox" style={{ marginTop: "4%", backgroundColor: "#7c7c7c" }}>
-                                {/* 加頭像 & 評價 */}
+                    <div class="bottom-container"> {/* 下方 */}
+
+                        {counters.map((_, index) => ( 
+                        <div key={index} class="row justify-content-center">
+                            <div class="col-10 UserBox" style={{textAlign:"center"}}>
+                                {order.d.participant[index].username} <br></br>
+                                {order.d.participant[index].total}元
                             </div>
-                        </div>
-                        <div class="row justify-content-center">
-                            <div class="col-10 UserBox"></div>
-                        </div>
-                        <div class="row justify-content-center">
-                            <div class="col-10 UserBox"></div>
-                        </div>
+                        </div>))}
+
                         <div class="row"></div>
                     </div>
 
