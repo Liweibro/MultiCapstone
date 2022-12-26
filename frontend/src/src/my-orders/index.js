282de727397ef6546ba088ad4b45a875db3e856a
yuchin1121 <83649237+yuchin1121@users.noreply.github.com>
2022-12-27 04:52:42 +0800

    Update index.js

--- frontend/src/src/my-orders/index.js
+++ frontend/src/src/my-orders/index.js
@@ -114,6 +114,7 @@ function FinishOrder(props) {
             </div>
 
             <div>
+                <div className="br_size"><br/></div>
                 <Button onClick={(event) => { props.onHide();} } id="btn-second" style={{margin: "10px"}}>完成取餐</Button>
             </div>
         </Modal.Body>
